5fcf0acb8aae54e1951042d90fafa6931fc4a461
VictorBlascoCano <101458913+VictorBlascoCano@users.noreply.github.com>
2025-12-22 15:26:13 +0100

    added theme mode toggle

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,11 @@
 import { SidebarTrigger } from "@/components/ui/sidebar";
+import { ModeToggle } from "./ModeToggle";
 
 const Header = () => {
 	return (
-		<header className="bg-sidebar h-10 mx-2 flex items-center px-2 rounded-lg border border-sidebar-border sticky top-2">
+		<header className="bg-sidebar h-10 mx-2 flex items-center gap-2 px-2 rounded-lg border border-sidebar-border sticky top-2">
 			<SidebarTrigger />
+			<ModeToggle />
 		</header>
 	);
 };
